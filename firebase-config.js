65e2448adb6b760ddb4cb0dcf2c2f0857bf988c6
Gustavo <gustavozambrano@gmail.com>
2026-05-07 19:16:20 -0300

    Adicionar configuracao Firebase e tokens

--- firebase-config.js
+++ firebase-config.js
@@ -1,2 +1,8 @@
-// TODO: preencher com credenciais do Firebase
-window.FIREBASE_CONFIG = {};
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyA1RuPFWwTKJR18Q3swbsXMAIRZBIz46S4",
+    authDomain: "bolao-copa-2026-2d772.firebaseapp.com",
+    projectId: "bolao-copa-2026-2d772",
+    storageBucket: "bolao-copa-2026-2d772.firebasestorage.app",
+    messagingSenderId: "673577543516",
+    appId: "1:673577543516:web:e4089a9472adc816550399"
+};
